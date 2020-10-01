3e1a32d219f6c2b141de99b34a0a2071e0f5e81b
Saumya610 <62713186+Saumya610@users.noreply.github.com>
2020-10-01 22:38:10 +0530

    Add files via upload

---
+++ PersonCodeFile.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace MyNs
+{
+    public class Person
+    {
+        private string Id;
+
+        public string ID
+        {
+            get { return Id; }
+            set { Id = value; }
+        }
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+    }
+}
